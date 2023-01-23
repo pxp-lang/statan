c57d9c96c9227fb97f5e973326195cc15c8b4f08
Ryan Chandler <ryangjchandler@gmail.com>
2023-01-23 18:22:06 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+struct Arguments {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    #[clap(about = "Analyse a file.")]
+    Analyse,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = Arguments::parse();
 }
