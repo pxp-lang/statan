cde8b1a199d42b15712947cd98d428ba5d5037ca
Ryan Chandler <ryangjchandler@gmail.com>
2023-01-24 20:05:50 +0000

    wip

--- src/definitions/collector.rs
+++ src/definitions/collector.rs
@@ -1,4 +1,4 @@
-use pxp_parser::{lexer::byte_string::ByteString, parser::ast::Statement};
+use pxp_parser::{lexer::byte_string::ByteString, parser::ast::{Statement, namespaces::{BracedNamespace, UnbracedNamespace}, identifiers::SimpleIdentifier, GroupUseStatement, UseStatement, Use}, traverser::Visitor, node::Node, downcast::{self, downcast}};
 
 #[derive(Debug)]
 pub struct DefinitionCollector {
@@ -15,6 +15,36 @@ impl DefinitionCollector {
     }
 
     pub fn scan(&mut self, ast: &mut Vec<Statement>) {
+
+    }
+}
+
+impl Visitor<()> for DefinitionCollector {
+    fn visit(&mut self, node: &mut dyn Node) -> Result<(), ()> {
+        if let Some(BracedNamespace { name: Some(SimpleIdentifier { value, .. }), .. }) = downcast::<BracedNamespace>(node) {
+            self.current_namespace = value.clone();
+        }
         
+        if let Some(UnbracedNamespace { name: SimpleIdentifier { value, .. }, .. }) = downcast::<UnbracedNamespace>(node) {
+            self.current_namespace = value.clone();
+        }
+
+        if let Some(GroupUseStatement { prefix, uses, .. }) = downcast::<GroupUseStatement>(node) {
+            for Use { name, .. } in uses {
+                let mut prefixed_name = prefix.value.clone();
+                prefixed_name.extend(b"\\");
+                prefixed_name.extend(&name.value.bytes);
+
+                self.imported_names.push(prefixed_name);
+            }
+        }
+
+        if let Some(UseStatement { uses, .. }) = downcast::<UseStatement>(node) {
+            for Use { name, .. } in uses {
+                self.imported_names.push(name.value.clone());
+            }
+        }
+
+        Ok(())
     }
 }
